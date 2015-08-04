c738035b25d53a56089bf7e88c6030c72c06c8c8
merlin <romain.dary@eoko.fr>
2015-08-04 09:00:00 +0200

    Init project

---
+++ test/Storage/DynamoDBv3Test.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Eoko\OAuth2\Test\Storage;
+
+use Eoko\OAuth2\Storage\DynamoDBv3;
+use PHPUnit_Framework_TestCase;
+
+class DynamoDBv3Test extends PHPUnit_Framework_TestCase
+{
+    public function testGetDefaultScope()
+    {
+        $client = $this->getMockBuilder('\Aws\DynamoDb\DynamoDbClient')
+            ->disableOriginalConstructor()
+            ->setMethods(array('query'))
+            ->getMock();
+
+        $return = $this->getMockBuilder('\Guzzle\Service\Resource\Model')
+            ->setMethods(array('count', 'toArray'))
+            ->getMock();
+
+        $data = array(
+            'Items' => array(),
+            'Count' => 0,
+            'ScannedCount'=> 0
+        );
+
+        $return->expects($this->once())
+            ->method('count')
+            ->will($this->returnValue(count($data)));
+
+        $return->expects($this->once())
+            ->method('toArray')
+            ->will($this->returnValue($data));
+
+        // should return null default scope if none is set in database
+        $client->expects($this->once())
+            ->method('query')
+            ->will($this->returnValue($return));
+
+        $storage = new DynamoDBv3($client);
+        $this->assertNull($storage->getDefaultScope());
+
+    }
+}
